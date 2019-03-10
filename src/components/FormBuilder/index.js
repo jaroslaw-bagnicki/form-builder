df7da219b9ff5be0f146f5dc0e0673b6f2a1cd44
Jarosław Bagnicki <jarek@bsgeo.pl>
2019-03-10 09:14:48 +0100

    Scaffold FormBuliderNode component

--- src/components/FormBuilder/index.js
+++ src/components/FormBuilder/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { arrayOf, number } from 'prop-types';
+import { formBuilderNodeType } from '../../types';
+import { FormBuilderNode } from '../';
 
-export const FormBuilder = () => (
+export const FormBuilder = ({ rootNodesList, nodes }) => (
   <div className="container">
-    --FormBuilder component --
+    {rootNodesList.map(nodeId => (<FormBuilderNode key={nodeId} id={nodeId} nodes={nodes} />))}
   </div>
 );
+
+FormBuilder.propTypes = {
+  rootNodesList: arrayOf(number.isRequired),
+  nodes: arrayOf(formBuilderNodeType)
+};
---
+++ src/components/FormBuilderNode/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+export class FormBuilderNode extends Component {
+  
+  render() {
+    return (
+      <div>
+        --FormBuilderNode component --
+      </div>
+
+    );
+  }
+}
