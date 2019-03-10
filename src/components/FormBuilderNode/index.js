0396d160ef64b34ee2cc07ebf426043338727457
Jarosław Bagnicki <jarek@bsgeo.pl>
2019-03-10 13:49:26 +0100

    Some work w condition render

--- src/components/FormBuilderNode/index.js
+++ src/components/FormBuilderNode/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import { arrayOf } from 'prop-types';
+import { arrayOf, number } from 'prop-types';
 import M from 'materialize-css';
-import { formBuilderNodeType, formBuilderConditonType, inputTypesDict } from '../../types';
+import { formBuilderNodeType, formBuilderConditonType, inputTypesDict, conditonTypesDict } from '../../types';
 import styles from './styles.module.css';
 
 export class FormBuilderNode extends Component {
@@ -9,10 +9,13 @@ export class FormBuilderNode extends Component {
   static propTypes = {
     nodes: arrayOf(formBuilderNodeType).isRequired,
     node: formBuilderNodeType.isRequired,
-    condition: formBuilderConditonType
+    condition: formBuilderConditonType,
+    parentId: number
   }
 
   state = {
+    conditionType: this.props.condition && this.props.condition.type,
+    conditionValue: this.props.condition && this.props.condition.value,
     question: this.props.node.question,
     type: this.props.node.type
   }
@@ -27,17 +30,33 @@ export class FormBuilderNode extends Component {
     M.AutoInit();
   }
 
-  renderCondition() {
-    // TODO
+  renderCondition(condition) {
+    const { type, value} = condition;
+    const parentId = this.props.parentId;
+    return (
+      <div className="row">
+        <div className="input-field col s6">
+          <label htmlFor={`${parentId}-condition`} className="active">Condition</label>
+          <select id={`${parentId}-condition`} name="condition" type="text" value={this.state.conditionType} onChange={this.handleChange}>
+            { Object.entries(conditonTypesDict).map(type => (
+              <option key={type[0]} value={type[0]} >{type[1]}</option>
+            ))}
+          </select>
+        </div>
+        <div className="input-field col s6">
+          <input id={`${parentId}-conditionValue`} name="conditionValue" type="text" value={this.state.conditionValue} onChange={this.handleChange} />             
+        </div>
+      </div>
+    );
   }
 
-  renderSubnodes(subnodes) {
+  renderSubnodes(subnodes, id) {
     return (
       <>
         { subnodes.map(subnode => {
           const node = this.props.nodes.find(node => node.id === subnode.nodeId);
           return (
-            <FormBuilderNode key={node.id} node={node} condition={subnode.condition} nodes={this.props.nodes} />
+            <FormBuilderNode key={node.id} node={node} parentId={id} condition={subnode.condition} nodes={this.props.nodes} />
           );
         })}
       </>
@@ -50,7 +69,7 @@ export class FormBuilderNode extends Component {
       <div className={styles.container}> 
         <div className="card large">
           <div className="card-content">
-            { condition && this.renderCondition() }
+            { condition && this.renderCondition(condition) }
             <div className="input-field">
               <label htmlFor={`${id}-question`}>Question</label>
               <input id={`${id}-question`} name="question" type="text" value={this.state.question} onChange={this.handleChange} />
@@ -68,7 +87,7 @@ export class FormBuilderNode extends Component {
         </div>
         { (subnodes.length > 0) && 
           <div className={styles.subnodesContainer}> 
-            {this.renderSubnodes(subnodes)}
+            {this.renderSubnodes(subnodes, id)}
           </div> }
       </div>
 
