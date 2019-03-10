8a1d84cc48712c9357460677b68b2eb18644bb39
Jarosław Bagnicki <jarek@bsgeo.pl>
2019-03-10 12:29:01 +0100

    Add reqursive form builder nodes populating

--- src/components/FormBuilderNode/index.js
+++ src/components/FormBuilderNode/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { arrayOf } from 'prop-types';
 import M from 'materialize-css';
 import { formBuilderNodeType, formBuilderConditonType, inputTypesDict } from '../../types';
+import styles from './styles.module.css';
 
 export class FormBuilderNode extends Component {
 
@@ -29,26 +30,46 @@ export class FormBuilderNode extends Component {
   renderCondition() {
     // TODO
   }
+
+  renderSubnodes(subnodes) {
+    return (
+      <>
+        { subnodes.map(subnode => {
+          const node = this.props.nodes.find(node => node.id === subnode.nodeId);
+          return (
+            <FormBuilderNode key={node.id} node={node} condition={subnode.condition} nodes={this.props.nodes} />
+          );
+        })}
+      </>
+    );
+  }
   
   render() {
-    const { node: {id}, condition } = this.props;
+    const { node: {id, subnodes}, condition } = this.props;
     return (
-      <div className="card">
-        <div className="card-content">
-          { condition && this.renderCondition() }
-          <div className="input-field">
-            <label htmlFor={`${id}-question`}>Question</label>
-            <input id={`${id}-question`} name="question" type="text" value={this.state.question} onChange={this.handleChange} />
-          </div>
-          <div className="input-field">
-            <label htmlFor={`${id}-type`} className="active">Type</label>
-            <select id={`${id}-type`} name="type" value={this.state.type} onChange={this.handleChange}>
-              { Object.entries(inputTypesDict).map(type => (
-                <option key={type[0]} value={type[0]} >{type[1]}</option>
-              )) }
-            </select>
+      <div className={styles.container}> 
+        <div className={['card', styles.card].join(' ')}>
+          <div className="card-content">
+            { condition && this.renderCondition() }
+            <div className="input-field">
+              <label htmlFor={`${id}-question`}>Question</label>
+              <input id={`${id}-question`} name="question" type="text" value={this.state.question} onChange={this.handleChange} />
+            </div>
+            <div className="input-field">
+              <label htmlFor={`${id}-type`} className="active">Type</label>
+              <select id={`${id}-type`} name="type" value={this.state.type} onChange={this.handleChange}>
+                { Object.entries(inputTypesDict).map(type => (
+                  <option key={type[0]} value={type[0]} >{type[1]}</option>
+                )) }
+              </select>
+            </div>
+  
           </div>
         </div>
+        { (subnodes.length > 0) && 
+          <div className={styles.subnodesContainer}> 
+            {this.renderSubnodes(subnodes)}
+          </div> }
       </div>
 
     );
